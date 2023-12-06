026e1e9accd2d215d0ed77734768792d3e40736a
Obnoviper <68788621+Obnoviper@users.noreply.github.com>
2023-12-06 15:22:37 -0500

    Add files via upload

--- COIS2020A3/COIS2020A3/Program.cs
+++ COIS2020A3/COIS2020A3/Program.cs
@@ -1,296 +1,243 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class SplayTree<T> : ICloneable
+public class Program
 {
-    private class Node
+    public static void Main(string[] args)
     {
-        public T Data;
-        public Node Left, Right;
-
-        public Node(T data)
-        {
-            Data = data;
-            Left = Right = null;
-        }
-    }
-
-    private Node root;
-
-    public void Insert(T item)
-    {
-        if (root == null)
-        {
-            root = new Node(item);
-            return;
-        }
-    }
-
-    public bool Remove(T item)
-    {
-        if (root == null)
-        {
-            // Tree is empty, item not found
-            return false;
-        }
-
-        // Step 1: Perform regular binary search to find the node containing the item
-        Splay(null, Access(item));
-
-        if (root.Data.Equals(item))
-        {
-            // Item found at the root, remove it
-            if (root.Left == null)
-            {
-                root = root.Right;
-            }
-            else
-            {
-                // Find the maximum item in the left subtree
-                Node maxLeft = FindMax(root.Left);
-                // Splay the maxLeft node to the root
-                Splay(maxLeft, Access(item));
-
-                // Attach the right subtree to the right of the left subtree
-                root.Left.Right = root.Right;
-                root = root.Left;
-            }
-
-            return true; // Item removed
-        }
-
-        return false; // Item not found
-    }
-
-    private Node FindMax(Node node)
-    {
-        while (node.Right != null)
-        {
-            node = node.Right;
-        }
-
-        return node;
-    }
-
-    public bool Contains(T item)
-    {
-        if (root == null)
-        {
-            // Tree is empty, item not found
-            return false;
-        }
-
-        // Step 1: Perform regular binary search to find the node containing the item
-        Splay(null, Access(item));
-
-        // Step 2: Check if the item is now at the root
-        return root.Data.Equals(item);
-    }
-
-    private Stack<Node> Access(T item)
-    {
-        Stack<Node> accessPath = new Stack<Node>();
-        Node current = root;
-
-        while (current != null)
-        {
-            accessPath.Push(current);
-
-            int compareResult = Comparer<T>.Default.Compare(item, current.Data);
-
-            if (compareResult == 0)
-            {
-                // Item found, break out of the loop
-                break;
-            }
-            else if (compareResult < 0)
-            {
-                // Item is smaller, move to the left subtree
-                current = current.Left;
-            }
-            else
-            {
-                // Item is larger, move to the right subtree
-                current = current.Right;
-            }
-        }
-
-        return accessPath;
-    }
-
-    private void Splay(Node p, Stack<Node> S)
-    {
-        while (S.Count > 0 && S.Peek() != p)
-        {
-            Node grandparent = S.Pop();
-            Node parent = S.Pop();
-
-            if (p == parent.Left)
-            {
-                if (grandparent.Left == parent)
-                {
-                    // Zig-Zig (Left-Left)
-                    RotateRight(grandparent);
-                    RotateRight(parent);
-                }
-                else
-                {
-                    // Zig-Zag (Right-Left)
-                    RotateRight(parent);
-                    RotateLeft(grandparent);
-                }
-            }
-            else
-            {
-                if (grandparent.Right == parent)
-                {
-                    // Zig-Zig (Right-Right)
-                    RotateLeft(grandparent);
-                    RotateLeft(parent);
-                }
-                else
-                {
-                    // Zig-Zag (Left-Right)
-                    RotateLeft(parent);
-                    RotateRight(grandparent);
-                }
-            }
-
-            // Push the grandparent back onto the stack for the next iteration
-            S.Push(grandparent);
-        }
-
-        // If p is still not at the root, perform a final rotation
-        if (S.Count == 0 && p != root)
-        {
-            if (p == root.Left)
-            {
-                // Zig (Left)
-                RotateRight(root);
-            }
-            else
-            {
-                // Zag (Right)
-                RotateLeft(root);
-            }
-        }
-    }
-
-    private void RotateLeft(Node x)
-    {
-        Node y = x.Right;
-        x.Right = y.Left;
-        y.Left = x;
-        x = y;
-    }
-
-    private void RotateRight(Node y)
-    {
-        Node x = y.Left;
-        y.Left = x.Right;
-        x.Right = y;
-        y = x;
-    }
-
-    public object Clone()
-    {
-        if (root == null)
-        {
-            return null; // Return null for an empty tree
-        }
-
-        SplayTree<T> clonedTree = new SplayTree<T>();
-        clonedTree.root = CloneNode(root);
-
-        return clonedTree;
-    }
-
-    private Node CloneNode(Node originalNode)
-    {
-        if (originalNode == null)
-        {
-            return null;
-        }
-
-        Node newNode = new Node(originalNode.Data);
-        newNode.Left = CloneNode(originalNode.Left);
-        newNode.Right = CloneNode(originalNode.Right);
-
-        return newNode;
-    }
-
-    public override bool Equals(object obj)
-    {
-        if (obj == null || GetType() != obj.GetType())
-        {
-            return false;
-        }
-
-        SplayTree<T> otherTree = (SplayTree<T>)obj;
-        return Equals(root, otherTree.root);
-    }
-
-    private bool Equals(Node node1, Node node2)
-    {
-        if (node1 == null && node2 == null)
-        {
-            return true;
-        }
-
-        if (node1 == null || node2 == null)
-        {
-            return false;
-        }
-
-        return
-            EqualityComparer<T>.Default.Equals(node1.Data, node2.Data) &&
-            Equals(node1.Left, node2.Left) &&
-            Equals(node1.Right, node2.Right);
-    }
-
-    public SplayTree<T> Undo()
-    {
-        if (root == null)
-        {
-            // Nothing to undo in an empty tree
-            return null;
-        }
-
-        // Clone the current tree for each attempt to undo
-        SplayTree<T> cloneTree = (SplayTree<T>)Clone();
-
-        // Try different rotations to bring the last inserted item to a leaf position
-        while (cloneTree.root.Right != null)
-        {
-            Node lastInsertedNode = cloneTree.FindMax(cloneTree.root.Left);
-            Stack<Node> accessPath = cloneTree.Access(lastInsertedNode.Data);
-            cloneTree.Splay(lastInsertedNode, accessPath);
-        }
-
-        // Remove the last inserted item at the leaf position
-        cloneTree.root = cloneTree.root.Left;
-
-        return cloneTree;
-    }
-
-    public static void Main()
-    {
-        // Example usage
+        // Create an instance of SplayTree
         SplayTree<int> tree = new SplayTree<int>();
 
+        // Use the Insert method
         tree.Insert(10);
-        tree.Insert(5);
-        tree.Insert(15);
+        tree.Insert(20);
+        tree.Insert(30);
 
-        Console.WriteLine(tree.Contains(5));  // Output: True
-        Console.WriteLine(tree.Contains(20)); // Output: False
+        // Use the Contains method
+        bool contains10 = tree.Contains(10);  // should return true
+        bool contains40 = tree.Contains(40);  // should return false
 
-        tree.Remove(10);
+        // Use the Remove method
+        tree.Remove(20);
 
+        // Create a clone of the tree
         SplayTree<int> clonedTree = (SplayTree<int>)tree.Clone();
-        Console.WriteLine(clonedTree.Equals(tree)); // Output: True
 
-        SplayTree<int> undoTree = tree.Undo();
-        Console.WriteLine(undoTree.Equals(clonedTree)); // Output: True
+        // Check if the cloned tree is equal to the original tree
+        bool isEqual = tree.Equals(clonedTree);  // should return true
+
+        // Undo the last insertion
+        tree.Undo();
+
+        // Add your own tests as needed
+    }
+
+    public class SplayTree<T> where T : IComparable<T>
+    {
+        private class Node<U>
+        {
+            public U item = default!;
+            public Node<U>? left, right;
+        }
+
+        private Node<T>? root = null;
+        private Node<T>? previousRoot = null;  // Track the previous root for undo operation
+
+        public SplayTree()
+        {
+            // Initialize the root node
+            root = new Node<T>();
+        }
+
+        private Stack<T> Access(T item)
+        {
+            Stack<T> path = new Stack<T>();
+            Node<T>? current = root;
+
+            while (current != null)
+            {
+                path.Push(current.item);
+
+                int compareResult = item.CompareTo(current.item);
+
+                if (compareResult == 0)
+                {
+                    break;
+                }
+                else if (compareResult < 0)
+                {
+                    current = current.left;
+                }
+                else
+                {
+                    current = current.right;
+                }
+            }
+
+            return path;
+        }
+
+        private void Splay(Node<T>? p, Stack<T> S)
+        {
+            while (S.Count > 0 && p != null)
+            {
+                T x = S.Pop();
+
+                int compareResult = x.CompareTo(p.item);
+
+                if (compareResult == 0)
+                {
+                    break;
+                }
+                else if (compareResult < 0)
+                {
+                    if (p.left != null)
+                    {
+                        Node<T>? q = p.left;
+                        p.left = q.right;
+                        q.right = p;
+                        p = q;
+                    }
+                }
+                else
+                {
+                    if (p.right != null)
+                    {
+                        Node<T>? q = p.right;
+                        p.right = q.left;
+                        q.left = p;
+                        p = q;
+                    }
+                }
+            }
+
+            root = p;
+        }
+
+        public void Insert(T item)
+        {
+            if (root == null)
+            {
+                root = new Node<T>() { item = item };
+                previousRoot = null;
+                return;
+            }
+
+            previousRoot = root;  // Save the current root for potential undo
+            Stack<T> path = Access(item);
+
+            Node<T>? newNode = new Node<T>() { item = item };
+
+            if (item.CompareTo(path.Peek()) < 0)
+            {
+                newNode.left = root.left;
+                newNode.right = root;
+                root.left = null;
+            }
+            else
+            {
+                newNode.right = root.right;
+                newNode.left = root;
+                root.right = null;
+            }
+
+            root = newNode;
+        }
+
+        public void Remove(T item)
+        {
+            Stack<T> path = Access(item);
+
+            if (item.CompareTo(path.Peek()) != 0)
+            {
+                return; // item not found
+            }
+
+            Node<T>? leftSubtree = root?.left;
+            Node<T>? rightSubtree = root?.right;
+
+            if (leftSubtree != null)
+            {
+                Node<T>? newRoot = leftSubtree;
+                root = leftSubtree;
+
+                Access(item); // move item to the root of the left subtree
+
+                root.right = rightSubtree; // attach the right subtree
+            }
+            else
+            {
+                root = rightSubtree; // no left subtree, simply move right subtree up
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            Stack<T> path = Access(item);
+            return (path.Count > 0 && item.CompareTo(path.Peek()) == 0);
+        }
+
+        public object Clone()
+        {
+            SplayTree<T> clonedTree = new SplayTree<T>();
+            CloneHelper(root, ref clonedTree.root);
+            return clonedTree;
+        }
+
+        private void CloneHelper(Node<T>? original, ref Node<T>? cloned)
+        {
+            if (original != null)
+            {
+                cloned = new Node<T>() { item = original.item };
+                CloneHelper(original.left, ref cloned.left);
+                CloneHelper(original.right, ref cloned.right);
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is SplayTree<T> otherTree)
+            {
+                return EqualsHelper(root, otherTree.root);
+            }
+            return false;
+        }
+
+        private bool EqualsHelper(Node<T>? node1, Node<T>? node2)
+        {
+            if (node1 == null && node2 == null)
+            {
+                return true;
+            }
+
+            if (node1 == null || node2 == null)
+            {
+                return false;
+            }
+
+            return node1.item.Equals(node2.item) &&
+                   EqualsHelper(node1.left, node2.left) &&
+                   EqualsHelper(node1.right, node2.right);
+        }
+
+        public SplayTree<T> Undo()
+        {
+            if (previousRoot == null)
+            {
+                throw new InvalidOperationException("Undo is only supported after an insertion.");
+            }
+
+            root = previousRoot;
+            previousRoot = null;  // Reset previousRoot after undoing the operation
+
+            return this;
+        }
+
+        public override int GetHashCode()
+        {
+            // Implement a hash code calculation based on the properties of your class.
+            // For simplicity, you can use the hash code of the root node's item.
+            return root?.item?.GetHashCode() ?? 0;
+        }
     }
 }
